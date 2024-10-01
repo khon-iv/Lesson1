3b3717b5afd4ce6918c84ac178e1814e3c6c264d
Khon Irina <khon.iv@dns-shop.ru>
2024-10-01 10:17:25 +1000

    Initial commit

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i < 101; i++)
+        {
+            if (i % 15 == 0)
+                Console.Write("FizzBizz ");
+            else if (i % 3 == 0)
+                Console.Write("Fizz ");
+            else if (i % 5 == 0)
+                Console.Write("Bizz ");
+            else
+                Console.Write(i + " ");
+        }
+    }
+}
